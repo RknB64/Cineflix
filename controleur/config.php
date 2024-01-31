ff30f5126e3969914d535e0f90cb59889fcf5c26
RknB64 <ercan.balikel86@gmail.com>
2024-01-31 18:39:10 +0100

    initial-commit

---
+++ controleur/config.php
@@ -0,0 +1,12 @@
+<?php
+
+
+ define('DB_USERNAME', 'root');
+ define('DB_PASSWORD', 'root@sio');
+ define('DB_DATABASE', '');
+ 
+ define('DB_HOST', 'localhost');
+
+define("RACINE", dirname(__DIR__));
+
+?>
---
+++ modele/bd_inc.php
@@ -0,0 +1,34 @@
+<?php
+
+abstract class DbConnect {
+	
+	protected static function executerRequete($sql) {
+
+		try {
+			$query = self::connexion()->prepare($sql);
+			$query->execute();
+			return $query;
+		}
+		catch(Exception $e) {
+			return $e->getMessage()."<br>Impossible de récupérer les données sur la table : messages";
+		}
+		
+		$query->closeCursor();
+		self::connexion()->close();
+	}
+
+	private static function connexion() {
+		
+		try {
+            $dsn = 'mysql:host=' . DB_HOST .';dbname=' . DB_DATABASE;
+
+            $conn = new PDO($dsn, DB_USERNAME, DB_PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'')); 
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (PDOException $e) {
+            die("Erreur de connexion PDO ");
+        }		
+	}
+	
+}
+
