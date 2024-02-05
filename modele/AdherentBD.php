c7496410164f9c85f2110b129aa2a1d666beec7b
Thibault <thibault.herve1@gmail.com>
2024-02-05 13:58:48 +0100

    Cree AdherentBD

--- modele/AdherentBD.php
+++ modele/AdherentBD.php
@@ -3,11 +3,20 @@
 class ArticleBD extends DbConnect {
 
     private $table = "Adherent";
+
+    private $db = self::connexion(); // ?? mettre dans un constructeur peut etre
+    
+    // ?? est ce qu'on met chaque element de la base comme ca, pour ensuite simplifier la manipulation des
+    // tableau retourné par cette classe ?
+    // par example: $adherentListe[0][$nom];
+    public $nom = "nom";
+    public $prenom = "prenom";
+    public $mail = "mail";
+    // ...
   
     public static function getAdherent() {
       $resultat;
       try {
-        $db = self::();
         $query = $db->prepare("SELECT * FROM $table");
         $query->execute();
 
@@ -24,9 +33,8 @@ class ArticleBD extends DbConnect {
   
   public static function getAdherentById(int $idA) {
       try {
-        $db = Connexion::getInstance();
         $query = $db->prepare("SELECT * FROM $table WHERE id = :idA");
-        $query->bindValue(':idA', $idA, PDO::PARAM_STR);
+        $query->bindValue(':idA', $idA, PDO::PARAM_INT);
         $query->execute();
 
         $ligne = $query->fetch(PDO::FETCH_ASSOC); while ($ligne) {
@@ -40,6 +48,10 @@ class ArticleBD extends DbConnect {
 
     $resultat;
   }
+
+  /* public static function creerAdherent(string $nom, string $prenom, ...) { */
+  /* public static function supprimerAdherent(int $idA) { */
+  /* public static function modifierAdherent(string $nom, string $prenom, ...) { */
 
 }
 
