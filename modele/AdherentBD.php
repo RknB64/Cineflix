6794fc419138f70ea7733c0356f82de31a28095f
Thibault <thibault.herve1@gmail.com>
2024-02-14 23:19:02 +0100

    Update et teste d'adherent

--- modele/AdherentBD.php
+++ modele/AdherentBD.php
@@ -1,43 +1,57 @@
 <?php
 
+include './DbConnect.php';
+
 class AdherentBD extends DbConnect {
 
-    private $table = "Adherent";
+    private static $table = "adherent";
 
-    public $nom = "nom";
-    public $prenom = "prenom";
-    public $mail = "mail";
-    public $password = "password";
-    public $points = "points";
-    public $date_creation = "date_creation";
-    public $compte = "compte";
+    public static $id               = "id";
+    public static $nom              = "nom";
+    public static $prenom           = "prenom";
+    public static $mail             = "mail";
+    public static $id_ville         = "id_ville";
+    public static $password         = "password";
+    public static $points           = "points";
+    public static $date_creation    = "date_creation";
+    public static $compte           = "compte";
 
-    public static function getAdherent() {
+
+
+    public static function getAdherent() : array 
+    {
+
       $db = self::connexion();
-      $resultat = [];
+      $resultat;
+
       try {
-        $query = self::$db->prepare("SELECT * FROM " . self::$table);
+        $query = $db->prepare("SELECT * FROM " . self::$table);
         $query->execute();
 
         $ligne = $query->fetch(PDO::FETCH_ASSOC);
         while ($ligne) {
             $resultat[] = $ligne;
             $ligne = $query->fetch(PDO::FETCH_ASSOC);
+
         }
       } catch (PDOException $e) {
           die( "Erreur !: " . $e->getMessage() );
       }
+
       $db = null;
       return $resultat;
     }
 
-    public static function getAdherentById(int $idA) {
-      $resultat = [];
+
+
+    public static function getAdherentById(int $idA) : array 
+    {
+      $resultat;
 
       $db = self::connexion();
 
       try {
-        $query = self::$db->prepare("SELECT * FROM " . self::$table . " WHERE id = :idA");
+        $query = $db->prepare("SELECT * FROM " . self::$table . " WHERE id = :idA");
         $query->bindValue(':idA', $idA, PDO::PARAM_INT);
         $query->execute();
 
@@ -54,84 +68,95 @@ class AdherentBD extends DbConnect {
       return $resultat;
     }
 
-    // TODO retourner vrai ou faux
-    public static function deleteAdherent(int $idA) { 
+
+
+    public static function deleteAdherent(int $idA): bool 
+    {
       
       $db = self::connexion();
+      $res = true;
 
       try {
-        $query = self::$db->prepare("DELETE FROM ".self::$table." WHERE id = :idA");
+        $query = $db->prepare("DELETE FROM ".self::$table." WHERE id = :idA");
         $query->bindValue(':idA', $idA, PDO::PARAM_INT);
-        $query->execute();
+        $res = $query->execute();
 
-        $ligne = $query->fetch(PDO::FETCH_ASSOC); while ($ligne) {
-            $resultat[] = $ligne;
-            $ligne = $query->fetch(PDO::FETCH_ASSOC);
-        }
       } catch (PDOException $e) {
-          die( "Erreur !: " . $e->getMessage() );
+          die("Erreur !: " . $e->getMessage());
+          $res = false;
       }
 
       $db = null;
 
+      return $res;
     }
+
 
 
     // TODO avoir le password hache, et determiner le format pour la date, retourner vrai ou faux
     // ?? sinon passer un objet adherent en parametre
-    public static function addAdherent($new_nom, $new_prenom, $new_id_ville, $new_mail, $new_password, $new_points, $new_date_creation, $new_compte) {
+    public static function addAdherent($new_nom, $new_prenom, $new_id_ville, $new_mail, 
+                                        $new_password, $new_points, $new_date_creation, $new_compte) : bool
+    {
       
-      $db = self::connexion();
+      $db   = self::connexion();
+      $res  = true;
 
       // colones à inserer
-      $colones = [self::$nom, self::$prenom, self::$mail, self::$password, self::$points, self::$date_creation, self::$compte];
+      $colones = [self::$nom, self::$prenom, self::$id_ville, self::$mail, self::$password, 
+                  self::$points, self::$date_creation, self::$compte];
 
       // formate les colones à insérer pour la requête et les ?
       $nomColones = implode(', ', $colones);
       $placeholders = implode(', ', array_fill(0, count($colones), '?'));
                 
       try {
-            $query = self::$db->prepare("INSERT INTO ".self::$table. " (" . $nomColones . ") VALUES (" .$placeholders. ") ");
+            $query = $db->prepare("INSERT INTO ".self::$table. " (" . $nomColones . ") 
+                                        VALUES (" .$placeholders. ") ");
 
-        $query->execute([$new_nom, $new_prenom, $new_id_ville, $new_mail, $new_password, $new_points, $new_date_creation, $new_compte]);
+            $res = $query->execute([$new_nom, $new_prenom, $new_id_ville, $new_mail, 
+                              $new_password, $new_points, $new_date_creation, $new_compte]); // TODO trouver un moyen de pas mettre ca a la main
 
-        $ligne = $query->fetch(PDO::FETCH_ASSOC); while ($ligne) {
-            $resultat[] = $ligne;
-            $ligne = $query->fetch(PDO::FETCH_ASSOC);
-        }
       } catch (PDOException $e) {
-          die( "Erreur !: " . $e->getMessage() );
+          die("Erreur !: " . $e->getMessage()); // TODO enlever les die()
+          $res = false;
       }
 
       $db = null;
+      return $res;
     }
 
-    // retourner quelque chose ? un objet adherent ?
-    public static function updateAdherent($idA, $new_nom, $new_prenom, $new_id_ville, $new_mail, $new_password, $new_points, $new_date_creation, $new_compte) {
+
+
+    // retourner un objet adherent ?
+    public static function updateAdherent($idA, $new_nom, $new_prenom, $new_id_ville, $new_mail, 
+                                          $new_password, $new_points, $new_date_creation, $new_compte) : bool 
+    {
 
       $db = self::connexion();
+      $res = true;
 
       // colones à inserer
-      $colones = [self::$nom, self::$prenom, self::$mail, self::$password, self::$points, self::$date_creation, self::$compte];
+      $colones = [self::$nom, self::$prenom, self::$id_ville, self::$mail, self::$password, self::$points, 
+                  self::$date_creation, self::$compte];
 
       // formate les colones à insérer pour la requête et les ?
-      $nomColones = implode(', ', $colones);
-      $placeholders = implode(', ', array_fill(0, count($colones), '?'));
+      $colonnes = implode('= ? , ', $colones) . "= ? ";
 
       try {
-        $query = self::$db->prepare("INSERT INTO ".self::$table." (".$nomColones.") VALUES (".$placeholders.") WHERE id=".$idA);
+        $query = $db->prepare("UPDATE ".self::$table." SET ".$colonnes." WHERE id=".$idA);
+        echo "UPDATE ".self::$table." SET ".$colonnes." WHERE id=".$idA;
+    
+        $res = $query->execute([$new_nom, $new_prenom, $new_id_ville, $new_mail, $new_password, 
+                                $new_points, $new_date_creation, $new_compte]);
 
-        $query->execute([$new_nom, $new_prenom, $new_id_ville, $new_mail, $new_password, $new_points, $new_date_creation, $new_compte]);
-
-        $ligne = $query->fetch(PDO::FETCH_ASSOC); while ($ligne) {
-            $resultat[] = $ligne;
-            $ligne = $query->fetch(PDO::FETCH_ASSOC);
-        }
       } catch (PDOException $e) {
           die( "Erreur !: " . $e->getMessage() );
+          $res = false;
       }
 
       $db = null;
+      return $res;
     }
 
 }
