f00f3729dcaff9e6197bf7b44cb7423cd3320209
RknB64 <ercan.balikel86@gmail.com>
2024-02-15 21:49:50 +0100

    base-functions

--- index.php
+++ index.php
@@ -3,11 +3,31 @@
 require ('./controleur/config.php');
 require ("./modele/FilmBD.php");
 
-$test = FilmBD::getFilms(DB_FILM_TABLE);
+$testGet = FilmBD::getFilms(DB_FILM_TABLE);
+$titre = 'John Wick';
+$desc = 'Un homme d\'une extrême concentration';
+$dateExpir = '2025-01-01 13:14:09';
+$dateSortie = '2024-01-01 13:14:09';
+$duree = '2H00';
+$idAffiche = 2;
+$etat = 'streaming';
+$testCreate = FilmBD::addFilm(DB_FILM_TABLE, $titre, $desc, $dateExpir, $dateSortie, $duree, $idAffiche, $etat);
+$testGetAll = FilmBD::getFilms(DB_FILM_TABLE);
+$testGetByTitle = FilmBD::getFilmByTitle(DB_FILM_TABLE, 'John Wick');
 
+echo 'TEST 1 : Get tout les films';
 echo '<pre>';
-print_r($test);
+print_r($testGet);
 echo '</pre>';
 
+echo 'TEST 2 : Get tout les films après Create';
+echo '<pre>';
+print_r($testGetall);
+echo '</pre>';
+
+echo 'TEST 3 : Get le film créé';
+echo '<pre>';
+print_r($testGetByTitle);
+echo '</pre>';
 
 ?>
--- modele/FilmBD.php
+++ modele/FilmBD.php
@@ -1,5 +1,6 @@
 <?php
 
+include_once("./controleur/config.php");
 require_once "./modele/DbConnect.php";
 
 class FilmBD extends DbConnect {
@@ -10,15 +11,15 @@ class FilmBD extends DbConnect {
     public static function addFilm($table, $titre, $desc, $droit, $date, $duree, $affiche, $etat) {
       try {
         $db = self::connexion();
-        $query = $db->prepare("INSERT INTO $table (titre, desc, droit, dateSortie, duree, affiche, etat)
-                              VALUES (:titre, :desc, :droit, :date, :dateSortie, :duree, :affiche, :etat)");
+        $query = $db->prepare("INSERT INTO $table (titre, description, duree, etat, id_affiche, date_sortie, date_expiration)
+                              VALUES (:titre, :desc, :duree, :etat, :affiche, :dateSortie, :droit)");
         $query->bindValue(':titre', $titre, PDO::PARAM_STR);
         $query->bindValue(':desc', $desc, PDO::PARAM_STR);
         $query->bindValue(':droit', $droit, PDO::PARAM_STR); //Apparemment on passe la date en string => on verra au test
         $query->bindValue(':dateSortie', $date, PDO::PARAM_STR);
         $query->bindValue(':duree', $duree, PDO::PARAM_STR);
         $query->bindValue(':affiche', $affiche, PDO::PARAM_INT); //Id_affiche int ou string ?
-        $query->bindValue(':etat', $etat, PDO::PARAM_BOOL); // cinéma ou streaming avec un boolean ?
+        $query->bindValue(':etat', $etat, PDO::PARAM_STR);
         $query->execute();
 
       } catch (PDOException $e) {
