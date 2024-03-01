c490dbd10647c9721f60846967c86379719c3927
Thibault <thibault.herve1@gmail.com>
2024-03-01 14:04:48 +0100

    Ajout colone type

--- src/vue/inscription.vue.php
+++ src/vue/inscription.vue.php
@@ -1,19 +1,20 @@
 <?php
 
 $fields = [
-  ["Nom"],
-  ["Prénom"],
-  ["Date de naissance"],
-  ["Adresse Postale"],
-  ["Email"],
-  ["Téléphone"],
-  ["Mot de passe"],
-  ["Retaper le mot de passe"]
+//  libellé                     type
+  ["Nom",                       "text"],
+  ["Prénom",                    "text"],
+  ["Date de naissance",         "text"],
+  ["Adresse Postale",           "text"],
+  ["Email",                     "email"],
+  ["Téléphone",                 "text"],
+  ["Mot de passe",              "password"],
+  ["Retaper le mot de passe",   "password"]
 ];
 
 ?>
 
-<div class="form-wrapper">
+<div class="form-wrapper container-fluid">
   <form action="" method="post">
       <?php
         foreach ($fields as $field) {
@@ -21,10 +22,11 @@ $fields = [
       ?>
           <div class="form-group">
             <label for='<?= $id ?>'> <?= $field[0] ?> </label>
-            <input class="form-control" type='text' id="<?= $id ?>">
+            <input class="form-control" type="<?$field[1]?>" id="<?= $id ?>">
           </div>
       <?php
         }
       ?>
   </form>
 </div>
+
